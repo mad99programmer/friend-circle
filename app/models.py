68497d6e2808ab80746e8d531ab763d61027164f
mitul <mitulms9@gmail.com>
2026-01-26 15:06:57 +0530

    Add vents table and migration

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import BigInteger, Column, String, DateTime
+from sqlalchemy import BigInteger, Column, String, DateTime, ForeignKey, Boolean
 from sqlalchemy.sql import func
 
 
@@ -8,4 +8,12 @@ class User(Base):
     id = Column(BigInteger, primary_key=True)
     handle = Column(String(50), unique=True, nullable=False)
     status = Column(String(20), default="active", nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class Vent(Base):
+    __tablename__="vents"
+    id = Column(BigInteger,primary_key=True)
+    content = Column(String,nullable=True)
+    user_id = Column(BigInteger,ForeignKey('users.id',ondelete="CASCADE"),nullable=False)
+    is_hidden = Column(Boolean, default=False, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
